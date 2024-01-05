929cd874eec0f5304594dc259ae9dabb878c7288
siddharth <dsiddharth25@gmail.com>
2024-01-05 12:32:15 +0530

    website update

--- assets/js/animation.js
+++
@@ -1,9 +0,0 @@
-const body = document.querySelector('body');
-
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 0) {
-    body.classList.add('scrolling');
-  } else {
-    body.classList.remove('scrolling');
-  }
-});
